a53035ac72ed967c6cc15c0926ffb2d194e9513c
Sho Mizutani <lowply@github.com>
2016-07-30 11:34:09 +0900

    Add bucket_policy.json

--- updateBucketConfig.sh
+++ updateBucketConfig.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws --profile private s3api put-bucket-website --bucket warmcolors.info --website-configuration file://bucket.json
+aws --profile private s3api put-bucket-website --bucket warmcolors.info --website-configuration file://bucket_website_hosting.json
+aws --profile private s3api put-bucket-policy --bucket warmcolors.info --policy file://bucket_policy.json
