2d8644811f243c258328424cee27244fa27365a9
Sho Mizutani <lowply@github.com>
2022-02-01 21:28:40 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-find . -name .DS_Store -delete -exec echo removed: {} \;
-
 aws \
-    --profile s3backup \
     s3 sync \
     --exclude "assets/img/*" \
     --exclude "assets/radio/*" \
