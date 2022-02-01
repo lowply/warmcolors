89b02e68c68d7505ea5513db6778332cd4e7b2f7
Sho Mizutani <lowply@github.com>
2022-02-01 14:50:26 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,6 +5,7 @@ find . -name .DS_Store -delete -exec echo removed: {} \;
 aws \
     --profile s3backup \
     s3 sync \
-    --exclude "assets/*" \
+    --exclude "assets/img/*" \
+    --exclude "assets/radio/*" \
     --delete \
     ./_site/ s3://lowply.org/warmcolors/
